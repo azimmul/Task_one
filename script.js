a43ed431bcf366e611fa02531da15afb903c4ce1
azimmul <azimmul@gmail.com>
2014-02-27 14:46:25 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$(document).ready(function() {
+
+  $('input').each(function() {
+
+    $(this).on('focus', function() {
+      $(this).parent('.css').addClass('active');
+    });
+
+    $(this).on('blur', function() {
+      if ($(this).val().length == 0) {
+        $(this).parent('.css').removeClass('active');
+      }
+    });
+
+    if ($(this).val() != '') $(this).parent('.css').addClass('active');
+
+  });
+
+});
